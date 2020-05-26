4b5796350706ea941b3670e572a4e5a1475f726d
Jordan Surgick <jordansurgick@aol.com>
2020-05-26 22:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ function printBadges(name) {
     for (let counter = 0; counter < name.length; counter++) {
         //[loop body]
         console.log(`Welcome ${name[counter]}! You are employee #${counter+1}.`);
-    }               
+    }
     return name
 }
 
 function tailsNeverFails() {
     let tailsCounter = 0;
-       //([condition]) 
+       //([condition])
     while (Math.random() >= 0.5) {
         //[loop body]
         tailsCounter++;
-        
+
     }
     return`You got ${tailsCounter} tails in a row!`;
 }
